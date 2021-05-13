faad945d762f3e6988e707b12df09be4fea1eaf5
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-05-13 11:07:46 +0530

    Add src/utils/authenticate.ts

---
+++ src/utils/authenticate.ts
@@ -0,0 +1,24 @@
+interface APIResponse extends Response {
+
+}
+
+export default async function authenticate(authUri: string, reqBody: any) {
+    try {
+        let response = await fetch(`/api/auth/${authUri}`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({reqBody})
+        });
+
+        response = await response.json();
+
+        return response;
+
+        // if (response.error) return response.error;
+        // if (response.body.error) return response.body;
+    } catch (err) {
+        return err;
+    }
+}
