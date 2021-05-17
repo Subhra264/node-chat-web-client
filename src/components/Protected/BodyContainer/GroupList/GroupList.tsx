be636b58717af1045ac0486f804424a6d12afb51
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-05-17 14:59:42 +0530

    Add GroupList component

---
+++ src/components/Protected/BodyContainer/GroupList/GroupList.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom"
+
+export interface Group {
+    id: string;
+    name: string;
+    grpImage?: string;
+    unreadMsg?: number;
+}
+
+interface GroupListProps {
+    groups: [Group] | undefined;
+}
+
+const GroupList: React.FC<GroupListProps> = (props: GroupListProps): JSX.Element => {
+    return (
+        <div id='group-list'>
+            {
+                props.groups?.map((group) => (
+                    <div className='group' key={group.id}>
+                        <Link to={group.id}>{group.name}</Link>
+                    </div>
+                ))
+            }
+            <div className='group'>+</div>
+        </div>
+    )
+};
+
+export default GroupList;
