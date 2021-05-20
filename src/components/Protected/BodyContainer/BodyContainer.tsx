4c16ea84f0ce1d95d6e95f1db6a9385cff8adebe
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-05-20 17:41:44 +0530

    Add code to BodyContainer.tsx to fetch data from server

--- src/components/Protected/BodyContainer/BodyContainer.tsx
+++ src/components/Protected/BodyContainer/BodyContainer.tsx
@@ -2,13 +2,39 @@ import { useEffect, useState } from "react";
 import ChannelList, { ChannelList as Channels } from "./ChannelList/ChannelList";
 import GroupList, { Group } from "./GroupList/GroupList";
 import './BodyContainer.scss';
+import { useParams } from "react-router";
 
-const BodyContainer: React.FC = (): JSX.Element => {
+interface URIParams {
+    groupId: string;
+    channelId: string;
+}
+
+const BodyContainer: React.FC = (props): JSX.Element => {
     const [groups, setGroups] = useState<[Group]>();
     const [channels, setChannels] = useState<Channels>();
-
+    const { groupId, channelId } = useParams<URIParams>();
+    
     useEffect(() => {
-        //Fetch the group data
+        fetch('/user/chat', {
+            method: 'Post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                groupId,
+                channelId
+            })
+        }).then((response) => {
+            return response.json();
+        }).then((result) => {
+            if (result.type === 'error') {
+                throw new Error(result.message);
+            }
+            setGroups(result.message.groups);
+            setChannels(result.message.channels);
+        }).catch((err: Error) => {
+            console.log(err);
+        })
     }, []);
     
     return (
