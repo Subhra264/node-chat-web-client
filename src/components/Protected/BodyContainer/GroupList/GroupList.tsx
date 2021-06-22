b81bfa048cbfa28bcaaddfebbe9abea436e8593c
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-06-22 16:53:05 +0530

    Add ModalBox to GroupList

--- src/components/Protected/BodyContainer/GroupList/GroupList.tsx
+++ src/components/Protected/BodyContainer/GroupList/GroupList.tsx
@@ -1,4 +1,7 @@
+import { MouseEventHandler, MouseEvent, useState, ChangeEvent, ChangeEventHandler } from 'react';
 import { Link } from 'react-router-dom';
+import { FormProps } from '../../../Form/Form';
+import ModalBox from '../../../ModalBox/ModalBox';
 
 export interface Group {
     id: string;
@@ -12,6 +15,53 @@ interface GroupListProps {
 }
 
 const GroupList: React.FC<GroupListProps> = (props: GroupListProps): JSX.Element => {
+    const [show, setShow] = useState(false);
+    const [groupName, setGroupName] = useState('');
+    
+    const onGroupNameChange: ChangeEventHandler<HTMLInputElement> = (ev: ChangeEvent<HTMLInputElement>) => {
+        setGroupName(ev.target.value);
+    };
+
+    const openModalBox: MouseEventHandler = (ev: MouseEvent) => {
+        ev.preventDefault();
+
+        setShow(true);
+    }
+
+    const createGroup: MouseEventHandler = (ev: MouseEvent) => {
+        ev.preventDefault();
+
+        fetch('/api/group', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': `Bearer `
+            },
+            body: JSON.stringify({
+                name: groupName
+            })
+        }).then(res => (
+            res.json()
+        )).then(result => {
+            console.log(result);
+        }).catch(err => {
+            console.log('Error creating new Group', err);
+        });
+    };
+
+    const formProps: FormProps = {
+        fields: {
+            groupName: {
+                type: 'text',
+                required: true,
+                placeholder: 'Group Name',
+                value: groupName,
+                onChange: onGroupNameChange
+            }
+        },
+        onSubmit: createGroup
+    }
+
     return (
         <div id='group-list'>
             {
@@ -21,7 +71,8 @@ const GroupList: React.FC<GroupListProps> = (props: GroupListProps): JSX.Element
                     </div>
                 ))
             }
-            <div className='group' id='add-group'>+</div>
+            <div className='group' id='add-group' onClick={openModalBox}>+</div>
+            <ModalBox show={show} form={formProps}/>
         </div>
     )
 };
