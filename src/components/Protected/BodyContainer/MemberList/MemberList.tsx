7b93b20098dcab7b180b1b51150419418a604c7e
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-08-17 23:44:09 +0530

    Fetch memberList of a group

--- src/components/Protected/BodyContainer/MemberList/MemberList.tsx
+++ src/components/Protected/BodyContainer/MemberList/MemberList.tsx
@@ -1,4 +1,9 @@
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useAccessToken } from "../../../../hooks/useUserSelector";
+import { GroupContext } from "../../../../utils/contexts";
+import { getRequest } from "../../../../utils/fetch-requests";
 
 interface Member {
     _id: string;
@@ -11,17 +16,41 @@ interface MemberListProps {
 }
 
 const MemberList: React.FC = (props): JSX.Element => {
+    const [memberList, setMemberList] = useState<Member[]>([]);
+    const { groupId } = useContext(GroupContext);
+    const accessToken = useAccessToken();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const successHandler = (result: any) => {
+            setMemberList(result);
+        };
+
+        const errorHandler = (err: Error) => {
+            console.log('Error fetching members', err.message);
+        };
+
+        // fetches the list of members in the group
+        getRequest(
+            `/groups/${groupId}/members`,
+            accessToken,
+            successHandler,
+            errorHandler,
+            dispatch
+        );
+    }, [groupId]);
+
     return (
         <div className='item-list'>
             <div id='invite-friends' className='item add-item'>
                 Invite friends
             </div>
             {
-                // props.members?.map((member: Member) => (
-                //     <div key={member._id} className='item'>
-                //         <Link to={`profile/${member._id}`}>{member.username}</Link>
-                //     </div>
-                // ))
+                memberList.map((member: Member) => (
+                    <div key={member._id} className='item'>
+                        <Link to={`/profile/${member._id}`}>{member.username}</Link>
+                    </div>
+                ))
             }
         </div>
     )
